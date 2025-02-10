41babf67fac2833d61a14b10cee66c5d271b64a4
joseantonioce05 <joseantonioce.06@gmail.com>
2025-02-10 15:55:58 -0400

    Ajustado la ventana del album

--- src/components/AlbumSongList/AlbumSongList.tsx
+++ src/components/AlbumSongList/AlbumSongList.tsx
@@ -15,6 +15,7 @@ const AlbumSongList = ({ albumId }: Props) => {
 
   const [songs, setSongs] = useState<Song[]>([]);
   const [album, setAlbum] = useState<Album| null>(null);
+  const [artist, setArtist] = useState(null);
 
   const { setSongName } = useSong();
 
@@ -25,6 +26,8 @@ const AlbumSongList = ({ albumId }: Props) => {
 
         const responseAlbum = await new AlbumService().getAlbum(albumId)
         setAlbum(responseAlbum.data.albumSearched);
+
+        setArtist(responseAlbum.data.albumSearched.artist.name)
 
       } catch (error) {
         console.log(error);
@@ -37,16 +40,16 @@ const AlbumSongList = ({ albumId }: Props) => {
 
   return <div className="album-detail-container">
     {album && 
-    <div key={album._id} className="grid lg:grid-cols-7 md:grid-cols-3 sm:grid-cols-2 max-sm:grid-cols-2">
+    <div key={album._id} className="grid lg:grid-cols-6 md:grid-cols-3 sm:grid-cols-2 max-sm:grid-cols-2">
     <div className="album-image">
       <img src={import.meta.env.VITE_API_URL + "album/image/" + album.image} alt="" />
     </div>
-    <div className="album-name">
+    <div className="album-name max-md:col-span-1 col-span-2">
       <h1>{album.title}</h1>
     </div>
   </div>
     }
-    <div className="description grid lg:grid-cols-12 md:grid-cols-8 max-md:grid-cols-5">
+    <div className="description grid max-xl:grid-cols-6 lg:grid-cols-6 md:grid-cols-8 max-md:grid-cols-6">
       <div className="numeral">
         <p>#</p>
       </div>
@@ -57,10 +60,14 @@ const AlbumSongList = ({ albumId }: Props) => {
       <div className="duration">
         <p>Duración</p>
       </div>
+
+      <div className="artist-name">
+        <p>Artista</p>
+      </div>
     </div>
     <hr />
     {songs.map((song) => (
-      <div className="song grid lg:grid-cols-12 md:grid-cols-8 max-md:grid-cols-5" id={song._id}>
+      <div className="song grid max-xl:grid-cols-6 lg:grid-cols-6 md:grid-cols-8 max-md:grid-cols-6" id={song._id}>
         <div className="song-track">
           <p> {song.track} </p>
         </div>
@@ -69,6 +76,9 @@ const AlbumSongList = ({ albumId }: Props) => {
         </div>
         <div className="song-duration">
           <p> {song.duration}</p>
+        </div>
+        <div>
+          <p>{ artist }</p>
         </div>
         <div className="song-play">
           <button onClick={() => {setSongName(song.file); localStorage.setItem('albumId', album!._id); localStorage.setItem('songName', song.name)}}><PlayArrowIcon /></button>
